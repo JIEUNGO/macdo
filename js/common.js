f41d2bc02103980e43b8624cbe86b54abeffd091
JIEUN <fulgidg@gmail.com>
2018-02-21 17:41:37 +0900

    d

--- js/common.js
+++ js/common.js
@@ -1,58 +1,108 @@
 $(function(){
-	var menu = $(".gnb>li");
-//방법1
-	menu.on("mouseover focus", function(){
-		var myThis = $(this);
+	/*var menu = $(".gnb>li"),
+		menuLast = $(".gnb>li:last-child"),
+		heading = $('h1'),
+		check = false;*/
 
-		myThis.closest("ul").find(">li>.submenu").stop().hide()
-		.end().find(">li.on").removeClass("on").off('hover').blur();
-        myThis.find(">.submenu").show();
-        $(this).addClass("on");
-					});
-	menu.on("mouseout blur", function(){
-		var myThis = $(this);
-		myThis.closest("ul").find(">li>.submenu").stop().hide()
-		.end().find(">li.on").removeClass("on").off('hover').blur();
+	//방법2
+	/*heading.on({
+		focusin : function(){
+			menu.removeClass('on');
+		}
 	});
-
-//방법2
-/*	menu.on({
+	menu.on({
 		mouseenter : function(){
-			var $actived = $(this).siblings('.on');
-			if($actived.length > 0){
-				$actived.removeClass('on').find('ol.submenu').hide();
+			var $on = $(this).siblings('.on');
+			if($on.length > 0){
+				$on.removeClass('on').find('ol.submenu').hide();
 				check = false;
 			}
 			$(this).addClass('on').find('ol.submenu').show();
 		},
-		 mouseleave : function(){
-                if( check ){
-                    $(this).find('ol.submenu').show();
-                }else{
-                    $(this).removeClass('on').find('ol.submenu').hide();
-                    
-                }
-            }
+		focusin : function(){
+			$(this).mouseenter();
+		},
+		mouseleave : function(){
+			if( check ){
+				$(this).find('ol.submenu').show();
+			}else{
+				$(this).removeClass('on').find('ol.submenu').hide();
+				
+			}
+		}
+	});
+	menuLast.on({
+		focusout : function(){
+			$(this).removeClass('on').blur();
+		}
+	});*/
+
+	var menuA = $(".gnb>.menu1>a"),
+		subMenu = $(".submenu"),
+		subMenuLastA = ('.submenu>li:last-child>a'),
+		$gnb = $(".gnb"),
+		$heading = $("h1>a");
+
+	menuA.on('mouseover focus', function(){
+		menuA.parent().removeClass('on');
+		$(this).parent().addClass('on').find('.submenu').show();
+	});
+	$gnb.on('mouseleave', function(){
+		menuA.next().hide();
+		menuA.parent('li').removeClass('on');
+	});
+	subMenuLastA.on('blur', function(){
+		menuA.parent('li').removeClass('on');
 	});
 
-	/*quick*/
-	var quk_menu = $(".quick > li");
 
-	quk_menu.on("mouseover focus", function(){
-		var myThis = $(this);
-		myThis.closest("ul").find(">li .quk_sub").stop().hide()
-		.end().find("li.on").removeClass("on").off('hover').blur();
-		myThis.find(">.quk_sub").show();
-        $(this).addClass("on");
+	var $menuWarp = $('.menu_wrap'),
+		$menuList = $menuWarp.find(' .cnt_menu'),
+		$menuListEl = $menuList.find('> dl dd a'),
+		$nav = $('.nav_menu')
+
+	$menuListEl.on('focus', function(){
+		//clearTimeout(setMenuOp)
+		$menuListEl.parents('.cnt_menu').removeClass('on');
+		$(this).parents('.cnt_menu').addClass('on');
 	});
-	quk_menu.on("mouseout blur", function(){
-		var myThis = $(this);
-		myThis.closest("ul").find(">li .quk_sub").stop().hide()
-		.end().find("li.on").removeClass("on").off('hover').blur();
+	$menuListEl.on('blur', function(){
+		//setMenuOp = setTimeout(function(){
+			$menuListEl.parents('.cnt_menu').removeClass('on');
+		//});
 	});
 
+/*	var $quick = $('.quick>li'),
+		$qukLast = $('.quick>li:last-child'),
+		$quk_sub = $('.quk_sub')*/
 
+	//방법2
+/*	$quick.on({
+		mouseenter : function(){
+			var $on = $(this).siblings('.on');
+			if($on.length > 0){
+				$on.removeClass('on').find('.quk_sub').hide();
+				check = false;
+			}
+			$(this).addClass('on').find('.quk_sub').show();
+		},
+		focusin : function(){
+			$(this).mouseenter();
+		},
+		mouseleave : function(){
+			if( check ){
+				$(this).find('.quk_sub').show();
 
-
+			}else{
+				$(this).removeClass('on').find('.quk_sub').hide();
+				
+			}
+		}
+	});
+	$qukLast.on({
+		focusout : function(){
+			$(this).removeClass('on');
+		}
+	})*/
 });
 
